581f8e9f5b694ddb55c21b45745688ac4bffa330
yasiralshawk <yasir.alshawk12@gmail.com>
2021-10-05 12:41:24 -0400

    added hello world class, hellow world

--- src/test/Yasir.java
+++ src/test/Yasir.java
@@ -2,5 +2,8 @@ package test;
 
 public class Yasir {
 
-    // hello my name is yasir
+    // hello my name is yasirm
+    public static void main(String[] args) {
+        System.out.println("hellow ");
+    }
 }
